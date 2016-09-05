1fbf78f7480cf4683885712b8c7b39e56fb6d41e
b-acon <b-acon@users.noreply.github.com>
2016-09-05 20:44:50 +0800

    Added ReadMode setting

--- Helpers/Settings.cs
+++ Helpers/Settings.cs
@@ -3,30 +3,45 @@ using Plugin.Settings.Abstractions;
 
 namespace Dustbuster.Helpers
 {
-  public static class Settings
-  {
-    private static ISettings AppSettings
-    {
-      get
-      {
-        return CrossSettings.Current;
-      }
-    }
+	public static class Settings
+	{
+		private static ISettings AppSettings
+		{
+			get
+			{
+				return CrossSettings.Current;
+			}
+		}
 
-    private const string EnableAnalyticsKey = "enable_analytics";
-	private static readonly bool EnableAnalyticsDefault = true;
+		private const string EnableAnalyticsKey = "enable_analytics";
+		private static readonly bool EnableAnalyticsDefault = true;
 
-    public static bool EnableAnalytics
-    {
-      get
-      {
-        return AppSettings.GetValueOrDefault(EnableAnalyticsKey, EnableAnalyticsDefault);
-      }
-      set
-      {
-        AppSettings.AddOrUpdateValue(EnableAnalyticsKey, value);
-      }
-    }
+		private const string EnableReadModeKey = "enable_readmode";
+		private static readonly bool EnableReadModeDefault = false;
 
-  }
+		public static bool EnableAnalytics
+		{
+			get
+			{
+				return AppSettings.GetValueOrDefault(EnableAnalyticsKey, EnableAnalyticsDefault);
+			}
+			set
+			{
+				AppSettings.AddOrUpdateValue(EnableAnalyticsKey, value);
+			}
+		}
+
+		public static bool EnableReadMode
+		{
+			get
+			{
+				return AppSettings.GetValueOrDefault(EnableReadModeKey, EnableReadModeDefault);
+			}
+			set
+			{
+				AppSettings.AddOrUpdateValue(EnableReadModeKey, value);
+
+			}
+		}
+	}
 }
