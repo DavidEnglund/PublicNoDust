9986a5c968f0539a9bd4d62eeb39f31b6ab03b68
Mona <MonaRe83@users.noreply.github.com>
2016-09-06 14:41:05 +0800

    Merged settings with navigation_drawer

---
+++ Models/ProductResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dustbuster.Models
+{
+    class ProductResult
+    {
+        public string ProductName { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Amount { get; set; }
+    }
+}
--- Views/DustbusterPage.xaml.cs
+++ Views/DustbusterPage.xaml.cs
@@ -1,15 +1,99 @@
 ﻿using Dustbuster;
+using Dustbuster.Models;
 using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace Dustbuster
 {
 	public partial class DustbusterPage : ContentPage
 	{
-		public DustbusterPage()
+        private bool panelShowing = false;
+        private List<ProductResult> products;
+
+        public DustbusterPage()
 		{
 			InitializeComponent();
-		}
+
+            populateListView();
+        }
+
+        private void populateListView()
+        {
+            products = new List<ProductResult>();
+            products.Add(new ProductResult() { ProductName = "Gluon", TimeStamp = DateTime.Now, Amount = "85000 L" });
+            products.Add(new ProductResult() { ProductName = "DustLig", TimeStamp = DateTime.Now, Amount = "120 L" });
+            products.Add(new ProductResult() { ProductName = "Gluon26", TimeStamp = DateTime.Now, Amount = "85 L" });
+            products.Add(new ProductResult() { ProductName = "Gluon", TimeStamp = DateTime.Now, Amount = "180 L" });
+            products.Add(new ProductResult() { ProductName = "DustJel", TimeStamp = DateTime.Now, Amount = "85 L" });
+            products.Add(new ProductResult() { ProductName = "Gluon", TimeStamp = DateTime.Now, Amount = "160 L" });
+            products.Add(new ProductResult() { ProductName = "DustJel", TimeStamp = DateTime.Now, Amount = "85 L" });
+            products.Add(new ProductResult() { ProductName = "Gluon", TimeStamp = DateTime.Now, Amount = "85 L" });
+            products.Add(new ProductResult() { ProductName = "DustLig", TimeStamp = DateTime.Now, Amount = "177 L" });
+
+            listViewHistory.ItemsSource = products;
+        }
+
+        private async void AnimatePanel()
+        {
+            // swap the state
+            this.PanelShowing = !PanelShowing;
+
+            if (this.PanelShowing)
+            {
+                btnDarkBackground.IsVisible = true;
+
+                foreach (var child in panel.Children)
+                {
+                    child.Scale = 0;
+                }
+
+                var rect = new Rectangle(0, panel.Y, panel.Width, panel.Height);
+                await this.panel.LayoutTo(rect, 250, Easing.CubicIn);
+
+                foreach (var child in panel.Children)
+                {
+                    await child.ScaleTo(1.2, 50, Easing.CubicIn);
+                    await child.ScaleTo(1, 50, Easing.CubicOut);
+                }
+            }
+            else
+            {
+                var rect = new Rectangle(-(panel.Width), panel.Y, panel.Width, panel.Height);
+                await this.panel.LayoutTo(rect, 200, Easing.CubicOut);
+
+                foreach (var child in panel.Children)
+                {
+                    child.Scale = 0;
+                }
+
+                btnDarkBackground.IsVisible = false;
+            }
+        }
+
+        
+        private void btnSideMenu_Clicked(object sender, EventArgs e)
+        {
+            AnimatePanel();
+        }
+
+        private void listViewItem_Clicked(object sender, EventArgs e)
+        {
+            //TODO  Make it display the corresponding result
+            throw new NotImplementedException();
+        }
+
+        private async void btnSettings_Clicked(object sender, EventArgs e)
+        {
+            //TODO  Navigate to Settings page
+            throw new NotImplementedException();
+        }
+
+        private async void btnAbout_Clicked(object sender, EventArgs e)
+        {
+            //TODO  Navigate to About page
+            throw new NotImplementedException();
+        }
 
         private async void TapCivilButton(object sender, EventArgs e)
         {
@@ -25,6 +109,13 @@ namespace Dustbuster
         {
             base.OnAppearing();
             NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+
+        private bool PanelShowing
+        {
+            get { return panelShowing; }
+            set { panelShowing = value; }
         }
     }
 }
