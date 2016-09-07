b234775f7eea388f8cff9af05e83680323973e34
michay <Coolkay1@users.noreply.github.com>
2016-09-08 05:35:40 +0800

    Small improvements and applied Ken's suggestions

--- Models/ProductResult.cs
+++ Models/ProductResult.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Dustbuster.Models
+namespace Dustbuster
 {
-    class ProductResult
+    public class ProductResult
     {
         public string ProductName { get; set; }
-        public DateTime TimeStamp { get; set; }
+        public DateTime DateCreated { get; set; }
         public string Amount { get; set; }
     }
 }
--- Views/DustbusterPage.xaml.cs
+++ Views/DustbusterPage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using Dustbuster;
-using Dustbuster.Models;
+using Dustbuster.ViewModels;
 using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
@@ -9,37 +9,23 @@ namespace Dustbuster
 	public partial class DustbusterPage : ContentPage
 	{
         private bool panelShowing = false;
-        private List<ProductResult> products;
+        private DustbusterViewModel dustbusterViewModel;
 
         public DustbusterPage()
 		{
-			InitializeComponent();
+            dustbusterViewModel = new DustbusterViewModel();
+            BindingContext = dustbusterViewModel;
 
-            populateListView();
+            InitializeComponent();
         }
 
-        private void populateListView()
-        {
-            products = new List<ProductResult>();
-            products.Add(new ProductResult() { ProductName = "Gluon", TimeStamp = DateTime.Now, Amount = "85000 L" });
-            products.Add(new ProductResult() { ProductName = "DustLig", TimeStamp = DateTime.Now, Amount = "120 L" });
-            products.Add(new ProductResult() { ProductName = "Gluon26", TimeStamp = DateTime.Now, Amount = "85 L" });
-            products.Add(new ProductResult() { ProductName = "Gluon", TimeStamp = DateTime.Now, Amount = "180 L" });
-            products.Add(new ProductResult() { ProductName = "DustJel", TimeStamp = DateTime.Now, Amount = "85 L" });
-            products.Add(new ProductResult() { ProductName = "Gluon", TimeStamp = DateTime.Now, Amount = "160 L" });
-            products.Add(new ProductResult() { ProductName = "DustJel", TimeStamp = DateTime.Now, Amount = "85 L" });
-            products.Add(new ProductResult() { ProductName = "Gluon", TimeStamp = DateTime.Now, Amount = "85 L" });
-            products.Add(new ProductResult() { ProductName = "DustLig", TimeStamp = DateTime.Now, Amount = "177 L" });
-
-            listViewHistory.ItemsSource = products;
-        }
 
         private async void AnimatePanel()
         {
-            // swap the state
-            this.PanelShowing = !PanelShowing;
+            // negate the state
+            this.panelShowing = !panelShowing;
 
-            if (this.PanelShowing)
+            if (this.panelShowing)
             {
                 btnDarkBackground.IsVisible = true;
 
@@ -79,19 +65,20 @@ namespace Dustbuster
 
         private void listViewItem_Clicked(object sender, EventArgs e)
         {
+            AnimatePanel();
+
             //TODO  Make it display the corresponding result
-            throw new NotImplementedException();
         }
 
         private async void btnSettings_Clicked(object sender, EventArgs e)
         {
-           
+            AnimatePanel();
             await Navigation.PushAsync(new SettingsPage());
         }
 
         private async void btnAbout_Clicked(object sender, EventArgs e)
         {
-           
+            AnimatePanel();
             await Navigation.PushAsync(new AboutPage());
         }
 
@@ -109,13 +96,6 @@ namespace Dustbuster
         {
             base.OnAppearing();
             NavigationPage.SetHasNavigationBar(this, false);
-        }
-
-
-        private bool PanelShowing
-        {
-            get { return panelShowing; }
-            set { panelShowing = value; }
         }
     }
 }
