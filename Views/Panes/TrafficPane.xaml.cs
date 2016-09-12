11d8a06b23267e2fed8f3cdbf3c7ee21fde10063
Jayden Spencer <jaydenspencer1@gmail.com>
2016-09-12 21:20:01 +0800

    add button selections to traffic page forgot to commit this bit

--- Views/Panes/TrafficPane.xaml.cs
+++ Views/Panes/TrafficPane.xaml.cs
@@ -10,21 +10,28 @@ namespace Dustbuster
 {
 	public partial class TrafficPane : AccordionPane
 	{
+		SelectButtonGroup myButtonGroup = new SelectButtonGroup();
+
 		public TrafficPane() : base("Traffic")
 		{
 			InitializeComponent();
+			myButtonGroup.AddButton(trafficedButton);
+			myButtonGroup.AddButton(nonTrafficedButton);
+			trafficedButton.Selected = false;
+			nonTrafficedButton.Selected = false;
 		}
 
-		public void OnWeatherButtonClicked(object sender, EventArgs args)
-		{
-			Owner.VisitPane(Owner.Panes["LocationArea"], Owner.Panes["Weather"]);
-			Title = Entry.Text;
-		}
 
-		public void OnLocationAreaButtonClicked(object sender, EventArgs args)
-		{
-			Owner.VisitPane(Owner.Panes["Weather"], Owner.Panes["LocationArea"]);
-			Title = Entry.Text;
-		}
+		//public void OnWeatherButtonClicked(object sender, EventArgs args)
+		//{
+		//	Owner.VisitPane(Owner.Panes["LocationArea"], Owner.Panes["Weather"]);
+		//	Title = Entry.Text;
+		//}
+
+		//public void OnLocationAreaButtonClicked(object sender, EventArgs args)
+		//{
+		//	Owner.VisitPane(Owner.Panes["Weather"], Owner.Panes["LocationArea"]);
+		//	Title = Entry.Text;
+		//}
 	}
 }
