a05a5c1a07469382379c5cf513a432cd29c7d17b
doritobandito <michael.white308@gmail.com>
2016-09-13 19:30:21 +0800

    Fixed AccordionHeader - Left aligned icon with padding and centered title text in relative layout

--- Views/AccordionHeader.cs
+++ Views/AccordionHeader.cs
@@ -3,56 +3,65 @@ using Xamarin.Forms;
 
 namespace Dustbuster
 {
-	public class AccordionHeader : ContentView
-	{
-		private RelativeLayout titleLayout;
-		private StackLayout imageHolder;
-		private Label titleLabel;
-		private Image iconImage;
+    public class AccordionHeader : ContentView
+    {
+        private RelativeLayout titleLayout;
+        private StackLayout imageHolder;
+        private Label titleLabel;
+        private Image iconImage;
 
 
-		public AccordionHeader(AccordionPane pane)
-		{
-			BackgroundColor = Color.Green;
-			HeightRequest = 50;
-			HorizontalOptions = LayoutOptions.FillAndExpand;
+        public AccordionHeader(AccordionPane pane)
+        {
+            BackgroundColor = Color.Green;
+            HeightRequest = 50;
+            HorizontalOptions = LayoutOptions.FillAndExpand;
 
-	
-			Content = (titleLayout = new RelativeLayout()
-			{
-				HorizontalOptions = LayoutOptions.Center,
-				VerticalOptions = LayoutOptions.Center
-			});
 
-			titleLayout.Children.Add((imageHolder = new StackLayout()
-			{
-				Padding = new Thickness(40, 0, 0, 0)
-			}), Constraint.Constant(100));
+            Content = (titleLayout = new RelativeLayout()
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            });
 
-			imageHolder.Children.Add((iconImage = new Image()
-			{
-				HeightRequest = 32
-			}));
-			titleLayout.Children.Add((titleLabel = new Label()
-			{
+            titleLayout.Children.Add((imageHolder = new StackLayout()
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Padding = new Thickness(20, 0, 0, 0)
+            }), widthConstraint: Constraint.RelativeToParent((parent) => { return parent.Width; }),
+                heightConstraint: Constraint.RelativeToParent((parent) => { return parent.Height; }));
 
-				FontSize = 20f,
-				TextColor = Color.White,
-				FontAttributes = FontAttributes.Bold
-			}), Constraint.Constant(100));
-		}
+            imageHolder.Children.Add((iconImage = new Image()
+            {
+                HeightRequest = 32,
+                HorizontalOptions = LayoutOptions.Start,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            }));
+            titleLayout.Children.Add((titleLabel = new Label()
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center,
+                FontSize = 20f,
+                TextColor = Color.White,
+                FontAttributes = FontAttributes.Bold
+            }), widthConstraint: Constraint.RelativeToParent((parent) => { return parent.Width; }),
+                heightConstraint: Constraint.RelativeToParent((parent) => { return parent.Height; }));
+        }
 
-		public string Title
-		{
-			get { return titleLabel.Text; }
-			set { this.titleLabel.Text = value; }
-		}
+        public string Title
+        {
+            get { return titleLabel.Text; }
+            set { this.titleLabel.Text = value; }
+        }
 
-		public ImageSource IconImage
-		{
-			get { return this.iconImage.Source; }
-			set { this.iconImage.Source = value; }
-		}
-	}
+        public ImageSource IconImage
+        {
+            get { return this.iconImage.Source; }
+            set { this.iconImage.Source = value; }
+        }
+    }
 }
 
