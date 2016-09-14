f445454c1771aa7b6bd36f0d33826d876226d96e
Rebecca <beckylum0214@gmail.com>
2016-09-14 16:03:10 +0800

    Product total test

---
+++ ViewModels/ProductCalculationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Dustbuster.ViewModels
+{
+    public class ProductCalculationViewModel
+    {
+
+        public ObservableCollection <ProductDescription> ProductSource { get; set; }// collection from the database
+        public int ApplicationsRequired { get; set; } // applications required
+        public int ApplicationRate { get; set; }  //mL per square m
+        public float ProductArea { get; set; }// bound to area calculations
+
+        public float TotalProductQty()
+        {
+            return (float)(ProductArea * ApplicationRate * ApplicationsRequired) / 1000; //mL to L
+        }
+    }
+}
