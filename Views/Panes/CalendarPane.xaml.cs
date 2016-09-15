8e4ba3b20b23fdb3f0fb73c5a017b8733a52ff31
Jayden Spencer <jaydenspencer1@gmail.com>
2016-09-15 16:25:03 +0800

    calendar pane is not part of the flow, can now access LocationArea Pane

--- Views/Panes/CalendarPane.xaml.cs
+++ Views/Panes/CalendarPane.xaml.cs
@@ -6,17 +6,56 @@ namespace Dustbuster
 {
 	public partial class CalendarPane : AccordionPane
 	{
+
+		//init traffic button group
+		SelectButtonGroup calendarButtonGroup = new SelectButtonGroup();
+
 		public CalendarPane() : base("Duration", "accordion_icon_calendar_over180.png")
         {
 			InitializeComponent();
 
 			Header.SetDynamicResource(StyleProperty, "calendarAccordionStyle");
 
+			//add trafficked and non trafficked button to button group
+			calendarButtonGroup.AddButton(under30Button);
+			calendarButtonGroup.AddButton(over30Under180Button);
+			calendarButtonGroup.AddButton(over180Button);
 		}
 
-		public void OnBackButtonClicked(object sender, EventArgs args)
+		//under 30 button click
+		public void under30Button_clicked(object sender, EventArgs e)
 		{
-			Navigation.PushAsync(new DustbusterPage());
+			calendarAnswer.Text = "Solution is for under 30 days";
+			Title = "Under 30 Days";
+
+			if (!Owner.IsPaneVisited(Owner.Panes["LocationArea"]))
+			{
+				Owner.VisitPane(Owner.Panes["LocationArea"]);
+			}
+		}
+
+		//over 30 or under 180 button click
+		public void over30Under180Button_clicked(object sender, EventArgs e)
+		{
+			calendarAnswer.Text = "Solution is for over 30 days";
+			Title = "Over 30 Days";
+
+			if (!Owner.IsPaneVisited(Owner.Panes["LocationArea"]))
+			{
+				Owner.VisitPane(Owner.Panes["LocationArea"]);
+			}
+		}
+
+		//over 180 button click
+		public void over180Button_clicked(object sender, EventArgs e)
+		{
+			calendarAnswer.Text = "Solution is for over 180 days";
+			Title = "Over 180 Days";
+
+			if (!Owner.IsPaneVisited(Owner.Panes["LocationArea"]))
+			{
+				Owner.VisitPane(Owner.Panes["LocationArea"]);
+			}
 		}
 	}
 }
