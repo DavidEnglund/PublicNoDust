a8f2e27f1b113a99f5ec192c83f20955ef19d921
b-acon <b-acon@users.noreply.github.com>
2016-09-18 16:12:47 +0800

    Added an Entry renderer to remove the focus line on Android

---
+++ Droid/Renderers/DroidEditRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Dustbuster.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using AndroidColor = Android.Graphics.Color;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(DroidEditRenderer))]
+namespace Dustbuster.Droid
+{
+	public class DroidEditRenderer : EntryRenderer
+	{
+		public DroidEditRenderer()
+		{
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null)
+			{
+				Control.SetBackgroundColor(AndroidColor.Transparent);
+			}
+		}
+	}
+}
