b8ccc9461eae495086c7823bc944743c9bef72ba
doritobandito <michael.white308@gmail.com>
2016-09-18 17:24:25 +0800

    Added pinch to zoom to ImageViewer

--- Views/Carousel/ImageViewer.cs
+++ Views/Carousel/ImageViewer.cs
@@ -7,7 +7,7 @@ using Xamarin.Forms;
 
 namespace Dustbuster.Views.Carousel
 {
-    class ImageViewer : ContentPage
+    public class ImageViewer : ContentPage
     {
         private Image _image;
 
@@ -26,13 +26,11 @@ namespace Dustbuster.Views.Carousel
                 HorizontalOptions = LayoutOptions.FillAndExpand,
                 VerticalOptions = LayoutOptions.FillAndExpand
             };
-            
+
             //Add image to initial layout and set constraints
-            layout.Children.Add(new Image
+            layout.Children.Add(new PinchToZoomContainer
             {
-                Source = _image.Source,
-                HorizontalOptions = LayoutOptions.CenterAndExpand,
-                VerticalOptions = LayoutOptions.CenterAndExpand
+                Content = CreateProductImage()
             },  Constraint.Constant(0),
                 Constraint.Constant(0),
                 Constraint.RelativeToParent((parent) => { return parent.Width; }),
@@ -57,6 +55,18 @@ namespace Dustbuster.Views.Carousel
             return layout;
         }
 
+        private Image CreateProductImage()
+        {
+            //Create new image, align it to the end and add the cross_icon source
+            Image productImage = new Image
+            {
+                Source = _image.Source,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            return productImage;
+        }
+
         /// <summary>
         /// This method creates the cross icon and adds the tap gesture to the icon.
         /// </summary>
@@ -67,7 +77,7 @@ namespace Dustbuster.Views.Carousel
             Image crossImage = new Image
             {
                 HorizontalOptions = LayoutOptions.End,
-                Source = ImageSource.FromFile("cross_icon.png")
+                Source = ImageSource.FromFile("cross_icon.png"),
             };
 
             //Create a new tap gesture, add the tap event and assign the tap gesture to the cross icon image
@@ -85,6 +95,85 @@ namespace Dustbuster.Views.Carousel
         private void CrossImageTapEvent(object sender, EventArgs e)
         {
             Navigation.PopModalAsync();
+        }
+
+        private void ProductImagePinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+        {
+            
+        }
+    }
+
+    public class PinchToZoomContainer : ContentView
+    {
+        private double currentScale;
+        private double startScale;
+        private double xOffset;
+        private double yOffset;
+
+        public PinchToZoomContainer()
+        {
+            var pinchGesture = new PinchGestureRecognizer();
+            pinchGesture.PinchUpdated += OnPinchUpdated;
+            GestureRecognizers.Add(pinchGesture);
+        }
+
+        public void OnPinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+        {
+            if (e.Status == GestureStatus.Started)
+            {
+                // Store the current scale factor applied to the wrapped user interface element,
+                // and zero the components for the center point of the translate transform.
+                startScale = Content.Scale;
+                Content.AnchorX = 0;
+                Content.AnchorY = 0;
+            }
+            if (e.Status == GestureStatus.Running)
+            {
+                // Calculate the scale factor to be applied.
+                currentScale += (e.Scale - 1) * startScale;
+                currentScale = Math.Max(1, currentScale);
+
+                // The ScaleOrigin is in relative coordinates to the wrapped user interface element,
+                // so get the X pixel coordinate.
+                double renderedX = Content.X + xOffset;
+                double deltaX = renderedX / Width;
+                double deltaWidth = Width / (Content.Width * startScale);
+                double originX = (e.ScaleOrigin.X - deltaX) * deltaWidth;
+
+                // The ScaleOrigin is in relative coordinates to the wrapped user interface element,
+                // so get the Y pixel coordinate.
+                double renderedY = Content.Y + yOffset;
+                double deltaY = renderedY / Height;
+                double deltaHeight = Height / (Content.Height * startScale);
+                double originY = (e.ScaleOrigin.Y - deltaY) * deltaHeight;
+
+                // Calculate the transformed element pixel coordinates.
+                double targetX = xOffset - (originX * Content.Width) * (currentScale - startScale);
+                double targetY = yOffset - (originY * Content.Height) * (currentScale - startScale);
+
+                // Apply translation based on the change in origin.
+                Content.TranslationX = targetX.Clamp(-Content.Width * (currentScale - 1), 0);
+                Content.TranslationY = targetY.Clamp(-Content.Height * (currentScale - 1), 0);
+
+                // Apply scale factor.
+                Content.Scale = currentScale;
+            }
+            if (e.Status == GestureStatus.Completed)
+            {
+                // Store the translation delta's of the wrapped user interface element.
+                xOffset = Content.TranslationX;
+                yOffset = Content.TranslationY;
+            }
+        }
+    }
+
+    public static class NumericExtenstions
+    {
+        public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
+        {
+            if (val.CompareTo(min) < 0) return min;
+            else if (val.CompareTo(max) > 0) return max;
+            else return val;
         }
     }
 }
