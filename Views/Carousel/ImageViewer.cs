2efd0427cea61b845835a33eabdcae672f71dfea
doritobandito <michael.white308@gmail.com>
2016-09-18 22:02:06 +0800

    Added panning and fixed android multiple gestures with custom render

--- Views/Carousel/ImageViewer.cs
+++ Views/Carousel/ImageViewer.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Dustbuster.Views.Carousel
@@ -30,6 +26,8 @@ namespace Dustbuster.Views.Carousel
             //Add image to initial layout and set constraints
             layout.Children.Add(new PinchToZoomContainer
             {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
                 Content = CreateProductImage()
             },  Constraint.Constant(0),
                 Constraint.Constant(0),
@@ -105,75 +103,103 @@ namespace Dustbuster.Views.Carousel
 
     public class PinchToZoomContainer : ContentView
     {
-        private double currentScale;
-        private double startScale;
-        private double xOffset;
-        private double yOffset;
+        private double startScale, currentScale, xOffset, yOffset, startX, startY;
 
         public PinchToZoomContainer()
         {
-            var pinchGesture = new PinchGestureRecognizer();
-            pinchGesture.PinchUpdated += OnPinchUpdated;
-            GestureRecognizers.Add(pinchGesture);
+            if (Device.OS != TargetPlatform.Android)
+            {
+                var pinchGesture = new PinchGestureRecognizer();
+                pinchGesture.PinchUpdated += OnPinchUpdated;
+                GestureRecognizers.Add(pinchGesture);
+
+                var panGesture = new PanGestureRecognizer();
+                panGesture.PanUpdated += OnPanUpdated;
+                GestureRecognizers.Add(panGesture);
+            }
         }
 
         public void OnPinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
         {
-            if (e.Status == GestureStatus.Started)
+            switch (e.Status)
             {
-                // Store the current scale factor applied to the wrapped user interface element,
-                // and zero the components for the center point of the translate transform.
-                startScale = Content.Scale;
-                Content.AnchorX = 0;
-                Content.AnchorY = 0;
-            }
-            if (e.Status == GestureStatus.Running)
-            {
-                // Calculate the scale factor to be applied.
-                currentScale += (e.Scale - 1) * startScale;
-                currentScale = Math.Max(1, currentScale);
+                case GestureStatus.Started:
+                    // Store the current scale factor applied to the wrapped user interface element,
+                    // and zero the components for the center point of the translate transform.
+                    startScale = Content.Scale;
+                    Content.AnchorX = 0;
+                    Content.AnchorY = 0;
 
-                // The ScaleOrigin is in relative coordinates to the wrapped user interface element,
-                // so get the X pixel coordinate.
-                double renderedX = Content.X + xOffset;
-                double deltaX = renderedX / Width;
-                double deltaWidth = Width / (Content.Width * startScale);
-                double originX = (e.ScaleOrigin.X - deltaX) * deltaWidth;
+                    break;
 
-                // The ScaleOrigin is in relative coordinates to the wrapped user interface element,
-                // so get the Y pixel coordinate.
-                double renderedY = Content.Y + yOffset;
-                double deltaY = renderedY / Height;
-                double deltaHeight = Height / (Content.Height * startScale);
-                double originY = (e.ScaleOrigin.Y - deltaY) * deltaHeight;
+                case GestureStatus.Running:
+                    // Calculate the scale factor to be applied.
+                    currentScale += (e.Scale - 1) * startScale;
+                    currentScale = Math.Max(1, currentScale);
 
-                // Calculate the transformed element pixel coordinates.
-                double targetX = xOffset - (originX * Content.Width) * (currentScale - startScale);
-                double targetY = yOffset - (originY * Content.Height) * (currentScale - startScale);
+                    // The ScaleOrigin is in relative coordinates to the wrapped user interface element,
+                    // so get the X pixel coordinate.
+                    double renderedX = Content.X + xOffset;
+                    double deltaX = renderedX / Width;
+                    double deltaWidth = Width / (Content.Width * startScale);
+                    double originX = (e.ScaleOrigin.X - deltaX) * deltaWidth;
 
-                // Apply translation based on the change in origin.
-                Content.TranslationX = targetX.Clamp(-Content.Width * (currentScale - 1), 0);
-                Content.TranslationY = targetY.Clamp(-Content.Height * (currentScale - 1), 0);
+                    // The ScaleOrigin is in relative coordinates to the wrapped user interface element,
+                    // so get the Y pixel coordinate.
+                    double renderedY = Content.Y + yOffset;
+                    double deltaY = renderedY / Height;
+                    double deltaHeight = Height / (Content.Height * startScale);
+                    double originY = (e.ScaleOrigin.Y - deltaY) * deltaHeight;
 
-                // Apply scale factor.
-                Content.Scale = currentScale;
-            }
-            if (e.Status == GestureStatus.Completed)
-            {
-                // Store the translation delta's of the wrapped user interface element.
-                xOffset = Content.TranslationX;
-                yOffset = Content.TranslationY;
+                    // Calculate the transformed element pixel coordinates.
+                    double targetX = xOffset - (originX * Content.Width) * (currentScale - startScale);
+                    double targetY = yOffset - (originY * Content.Height) * (currentScale - startScale);
+
+                    // Apply translation based on the change in origin.
+                    Content.TranslationX = Math.Min(0, Math.Max(targetX, -Content.Width * (currentScale - 1)));
+                    Content.TranslationY = Math.Min(0, Math.Max(targetY, -Content.Height * (currentScale - 1)));
+
+                    // Apply scale factor.
+                    Content.Scale = currentScale;
+
+                    break;
+
+                case GestureStatus.Completed:
+                    // Store the translation delta's of the wrapped user interface element.
+                    xOffset = Content.TranslationX;
+                    yOffset = Content.TranslationY;
+
+                    break;
             }
         }
-    }
 
-    public static class NumericExtenstions
-    {
-        public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
+        public void OnPanUpdated(object sender, PanUpdatedEventArgs e)
         {
-            if (val.CompareTo(min) < 0) return min;
-            else if (val.CompareTo(max) > 0) return max;
-            else return val;
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    startX = e.TotalX;
+                    startY = e.TotalY;
+                    Content.AnchorX = 0;
+                    Content.AnchorY = 0;
+
+                    break;
+
+                case GestureStatus.Running:
+                    var maxTranslationX = Content.Scale * Content.Width - Content.Width;
+                    Content.TranslationX = Math.Min(0, Math.Max(-maxTranslationX, xOffset + e.TotalX - startX));
+
+                    var maxTranslationY = Content.Scale * Content.Height - Content.Height;
+                    Content.TranslationY = Math.Min(0, Math.Max(-maxTranslationY, yOffset + e.TotalY - startY));
+
+                    break;
+
+                case GestureStatus.Completed:
+                    xOffset = Content.TranslationX;
+                    yOffset = Content.TranslationY;
+
+                    break;
+            }
         }
     }
 }
