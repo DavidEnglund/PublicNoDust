d8f1a1a1ba54d2a73c265237f28bd71d99293969
doritobandito <michael.white308@gmail.com>
2016-09-19 17:02:15 +0800

    ContactInfoEntry changes keyboard input relative to ContactMethodPicker selection

--- Views/SettingsPage.xaml.cs
+++ Views/SettingsPage.xaml.cs
@@ -14,6 +14,20 @@ namespace Dustbuster
 			this.BindingContext = new SettingsViewModel();
 
             InitializeComponent();
+
+            ContactMethodPicker.SelectedIndexChanged += ContactMethodPicker_SelectedIndexChanged;
+        }
+
+        private void ContactMethodPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ContactMethodPicker.SelectedIndex == 1)
+            {
+                ContactInfoEntry.Keyboard = Keyboard.Email;
+            }
+            else
+            {
+                ContactInfoEntry.Keyboard = Keyboard.Numeric;
+            }
         }
     }
 }
