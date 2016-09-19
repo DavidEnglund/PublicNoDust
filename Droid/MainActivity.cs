7e1f08d70c762c63427bdbd5855eb0499f60f95f
Jayden Spencer <jaydenspencer1@gmail.com>
2016-09-19 13:00:47 +0800

    Lock orientation to portrait on android

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -10,7 +10,8 @@ using Android.OS;
 
 namespace Dustbuster.Droid
 {
-	[Activity(Label = "Dustbuster.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "Dustbuster.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+	          ScreenOrientation = ScreenOrientation.Portrait)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle bundle)
