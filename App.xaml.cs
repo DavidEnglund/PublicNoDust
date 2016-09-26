399fd2b642ee7b34588c21e43dce2b26de3ffa7f
b-acon <b-acon@users.noreply.github.com>
2016-09-26 23:28:32 +0800

    Finally removed unneeded code

--- App.xaml.cs
+++ App.xaml.cs
@@ -87,35 +87,6 @@ namespace Dustbuster
 		{
 			// Handle when your app resumes
 		}
-
-		public class ListDataViewCell : ViewCell
-		{
-			public ListDataViewCell()
-			{
-				var label = new Label()
-				{
-					//Font = Font.SystemFontOfSize(NamedSize.Default),
-					TextColor = Color.Blue
-				};
-				label.SetBinding(Label.TextProperty, new Binding("TextValue"));
-				label.SetBinding(Label.ClassIdProperty, new Binding("DataValue"));
-				View = new StackLayout()
-				{
-					Orientation = StackOrientation.Vertical,
-					VerticalOptions = LayoutOptions.StartAndExpand,
-					Padding = new Thickness(12, 8),
-					Children = { label }
-				};
-			}
-		}
-
-		public class SimpleObject
-		{
-			public string TextValue
-			{ get; set; }
-			public string DataValue
-			{ get; set; }
-		}
 	}
 
 	public enum IndustryOptions { Civil, Mining, None };
