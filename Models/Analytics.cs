85c35b07f36f826e356ab8c6e788defb154c26c2
Erdal Erkilic <erdal.mybooks@hotmail.com>
2016-10-05 12:13:34 +0800

    I just made a class for Analytics, it doesnt have anything in it yet. I need to re-clone the project and dont want to re-download my nugget so commiting for now

---
+++ Models/Analytics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.GoogleAnalytics;
+
+namespace Dustbuster.Models
+{
+    class Analytics
+    {
+        public Analytics()
+        {
+
+        }
+
+        public void SetUp()
+        {
+            GoogleAnalytics.Current.Config.TrackingId = "UA-11111111-1";
+            GoogleAnalytics.Current.Config.AppId = "AppID";
+            GoogleAnalytics.Current.Config.AppName = "TEST";
+            GoogleAnalytics.Current.Config.AppVersion = "1.0.0.0";
+        }
+    }
+}
