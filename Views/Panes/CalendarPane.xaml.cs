f536ae8ac47e96a3d69a9706b17828f70d19a57f
michay <Coolkay1@users.noreply.github.com>
2016-10-11 03:07:44 +0800

    3 Calendar buttons on non trafficked selection are now aligned. Image scaling issue remains on both large and small button sizes

--- Views/Panes/CalendarPane.xaml.cs
+++ Views/Panes/CalendarPane.xaml.cs
@@ -9,12 +9,16 @@ namespace Dustbuster
 
 		//init calendar button group
 		SelectButtonGroup calendarButtonGroup = new SelectButtonGroup();
+        SelectImageButton under30Button = new SelectImageButton();
+        SelectImageButton over30Under180Button = new SelectImageButton();
+        SelectImageButton over180Button = new SelectImageButton();
 
-		public CalendarPane() : base("Duration", "unselectedNone.png")
+        public CalendarPane() : base("Duration", "unselectedNone.png")
 		{
 			InitializeComponent();
+            createButtons();
 
-			Header.SetDynamicResource(StyleProperty, "calendarAccordionStyle");
+            Header.SetDynamicResource(StyleProperty, "calendarAccordionStyle");
 
 			//add calendar buttons to button group
 			calendarButtonGroup.AddButton(under30Button);
@@ -24,8 +28,10 @@ namespace Dustbuster
 
 		public override void OnPaneExpanded()
 		{
-			//trafficked choice, does not display over 180 day choice
-			over180Button.IsVisible = (App.TrafficOption == TrafficOptions.TraffickedArea) ? false : true;
+            //Recreate buttons
+            removeButtons();
+            createButtons();
+
 			//change over30Under180 button icon (unslected and selected icon)
 			over30Under180Button.SelectedImage = (App.TrafficOption == TrafficOptions.TraffickedArea) ? "choice_calendar_over30.png" : "choice_calendar_under180.png";
 			over30Under180Button.UnselectedImage = (App.TrafficOption == TrafficOptions.TraffickedArea) ? "choice_calendar_over30.png" : "choice_calendar_under180.png";
@@ -36,6 +42,101 @@ namespace Dustbuster
 				calendarButtonGroup.Selected = null;
 			}
 		}
+
+
+        private void createButtons()
+        {
+            double sizePercentage = 0;
+
+            setSharedProperties(under30Button, over30Under180Button, over180Button);
+
+            //under 30 days button
+            under30Button.SelectedImage = "choice_calendar_under30.png";
+            under30Button.UnselectedImage = "choice_calendar_under30.png";
+            under30Button.Clicked += under30Button_clicked;
+
+            //under 180 days button
+            over30Under180Button.SelectedImage = "choice_calendar_over30.png";
+            over30Under180Button.UnselectedImage = "choice_calendar_over30.png";
+            over30Under180Button.Clicked += over30Under180Button_clicked;
+
+            //over 180 days button
+            over180Button.SelectedImage = "choice_calendar_over180.png";
+            over180Button.UnselectedImage = "choice_calendar_over180.png";
+            over180Button.Clicked += over180Button_clicked;
+            over180Button.IsVisible = (App.TrafficOption == TrafficOptions.TraffickedArea) ? false : true;  //trafficked choice does not display over 180 days choice
+
+
+            //Adding the buttons to the layout
+            //ConstraintX, ConstraintY, ConstraintWidth, ConstraintHeight
+
+            if (over180Button.IsVisible)    //if all three options are visible
+            {
+                sizePercentage = 0.28;   // 3 buttons - intentionnally not 1/3
+
+                //under 180 days button
+                layoutDurationButtons.Children.Add(over30Under180Button,
+                    Constraint.RelativeToParent((parent) => { return parent.Width * 0.5 - parent.Width * sizePercentage / 2; }),    // Centered
+                    Constraint.Constant(0),
+                    Constraint.RelativeToParent((parent) => { return (parent.Width * sizePercentage); }),
+                    Constraint.RelativeToParent((parent) => { return (parent.Width * sizePercentage); })
+                    );
+            }
+            else
+            {
+                sizePercentage = 0.4;   // 2 buttons - intentionnally not 1/2
+
+                //under 180 days button
+                layoutDurationButtons.Children.Add(over30Under180Button,
+                    Constraint.RelativeToParent((parent) => { return parent.Width - parent.Width * sizePercentage - 20; }),     // separated by 20 pixels from the end of the screen
+                    Constraint.Constant(0),
+                    Constraint.RelativeToParent((parent) => { return (parent.Width * sizePercentage); }),
+                    Constraint.RelativeToParent((parent) => { return (parent.Width * sizePercentage); })
+                    );
+            }
+            //under 30 days button
+            layoutDurationButtons.Children.Add(under30Button,
+                Constraint.Constant(20),
+                Constraint.Constant(0),
+                Constraint.RelativeToParent((parent) => { return (parent.Width * sizePercentage); }),
+                Constraint.RelativeToParent((parent) => { return (parent.Width * sizePercentage); })
+                );
+            //over 180 days button
+            layoutDurationButtons.Children.Add(over180Button,
+                Constraint.RelativeToParent((parent) => { return parent.Width - parent.Width * sizePercentage - 20; }),    // separated by 20 pixels from the end of the screen
+                Constraint.Constant(0),
+                Constraint.RelativeToParent((parent) => { return (parent.Width * sizePercentage); }),
+                Constraint.RelativeToParent((parent) => { return (parent.Width * sizePercentage); })
+                );
+        }
+
+        private void setSharedProperties(params SelectImageButton[] buttons)
+        {
+            //Defines the appearance of the buttons.
+            foreach (SelectImageButton btn in buttons)
+            {
+                btn.SelectedBorderColor = Color.Black;
+                btn.SelectedBorderWidth = 0;
+
+                btn.HoldBorderWidth = 0;
+                btn.HoldBorderColor = Color.Black;
+
+                btn.UnselectedBorderColor = Color.White;
+                btn.UnselectedBorderWidth = 0;
+
+                btn.SetDynamicResource(VisualElement.StyleProperty, "selectableButtonStyle");
+
+                btn.Padding = 26;     //Doesn't seem to affect anything but was in the original code
+            }
+        }
+
+        private void removeButtons()
+        {
+            layoutDurationButtons.Children.Remove(under30Button);
+            layoutDurationButtons.Children.Remove(over30Under180Button);
+            layoutDurationButtons.Children.Remove(over180Button);
+        }
+
 
 		//under 30 button click
 		public void under30Button_clicked(object sender, EventArgs e)
@@ -76,6 +177,7 @@ namespace Dustbuster
 			//over 180 days always goesto location area
 			goToLocationAreaPane();
         }
+
 
 		private void goToWeatherPane()
 		{
