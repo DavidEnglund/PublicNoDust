60f244abf8fd6583a9d2af416d4e186c2457076e
ChuckUFarley <ChuckUFarley@users.noreply.github.com>
2016-10-17 18:12:46 +0800

    added UserInfo model
    WIP will add a single row table or something to the db asap

---
+++ Models/UserInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dustbuster.Models
+{
+    class UserInfo
+    {
+        public int SPId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool InfoChanged { get; set; }
+
+        public UserInfo()
+        {
+
+        }
+    }
+}
