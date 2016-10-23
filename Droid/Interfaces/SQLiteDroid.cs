b08cdca67d8d44ed97f06e4d7efee8dd65e2c565
ChuckUFarley <ChuckUFarley@users.noreply.github.com>
2016-10-23 15:41:47 +0800

    switched output path to public folder

--- Droid/Interfaces/SQLiteDroid.cs
+++ Droid/Interfaces/SQLiteDroid.cs
@@ -14,8 +14,9 @@ namespace Dustbuster.Droid
         {
             //see override below to accomodate multiple databases
 
-            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // Documents folder
-
+            //string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // Documents folder
+            
+            string documentsPath = global::Android.OS.Environment.ExternalStorageDirectory.Path;
             var productDBfilename = "ProductDB.db3";
             var jobDBfilename = "JobDB.db3";
 
@@ -35,8 +36,9 @@ namespace Dustbuster.Droid
         public SQLiteConnection GetConnection(string dbName)
         {
             //should make db filenames a string resource or something
-            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // Documents folder
-         
+            //string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // Documents folder
+            string documentsPath = global::Android.OS.Environment.ExternalStorageDirectory.Path;
+
 
             string dbFileName = dbName;
 
