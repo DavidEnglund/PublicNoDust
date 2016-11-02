1f6de7fbfb20dcc8b9abe13ec949d63a915c7c28
b-acon <b-acon@users.noreply.github.com>
2016-11-02 17:37:31 +0800

    Moved nested class under parent class definition

--- Controls/AccordionPane.cs
+++ Controls/AccordionPane.cs
@@ -5,6 +5,73 @@ namespace Dustbuster
 {
 	public class AccordionPane : ContentView
 	{
+		public class AccordionHeader : ContentView
+		{
+			private RelativeLayout titleLayout;
+			private StackLayout imageHolder;
+			private Label titleLabel;
+			private Image iconImage;
+
+
+			public AccordionHeader(AccordionPane pane)
+			{
+				HeightRequest = 50;
+				HorizontalOptions = LayoutOptions.FillAndExpand;
+
+
+				Content = (titleLayout = new RelativeLayout()
+				{
+					HorizontalOptions = LayoutOptions.FillAndExpand,
+					VerticalOptions = LayoutOptions.FillAndExpand
+				});
+
+				titleLayout.Children.Add((imageHolder = new StackLayout()
+				{
+					VerticalOptions = LayoutOptions.FillAndExpand,
+					WidthRequest = 60
+				}), heightConstraint: Constraint.RelativeToParent((parent) => { return parent.Height; }));
+
+				StackLayout imageContainer;
+				imageHolder.Children.Add(imageContainer = new StackLayout()
+				{
+					HorizontalOptions = LayoutOptions.FillAndExpand,
+					VerticalOptions = LayoutOptions.FillAndExpand,
+					Padding = new Thickness(20, 0, 0, 0)
+				});
+
+				imageContainer.Children.Add((iconImage = new Image()
+				{
+					HeightRequest = 32,
+					HorizontalOptions = LayoutOptions.Center,
+					VerticalOptions = LayoutOptions.CenterAndExpand
+				}));
+				titleLayout.Children.Add((titleLabel = new Label()
+				{
+					HorizontalOptions = LayoutOptions.Center,
+					VerticalOptions = LayoutOptions.Center,
+					HorizontalTextAlignment = TextAlignment.Center,
+					VerticalTextAlignment = TextAlignment.Center,
+					FontSize = 20f,
+					TextColor = Color.White,
+					FontAttributes = FontAttributes.Bold
+				}), widthConstraint: Constraint.RelativeToParent((parent) => { return parent.Width; }),
+					heightConstraint: Constraint.RelativeToParent((parent) => { return parent.Height; }));
+			}
+
+			public string Title
+			{
+				get { return titleLabel.Text; }
+				set { this.titleLabel.Text = value; }
+			}
+
+			public ImageSource IconImage
+			{
+				get { return this.iconImage.Source; }
+				set { this.iconImage.Source = value; }
+			}
+		}
+
+
 		public static readonly BindableProperty TitleProperty = BindableProperty.Create("Title", typeof(string), typeof(AccordionPane), "", propertyChanged: OnTitleChanged);
 		public static readonly BindableProperty ImageProperty = BindableProperty.Create("Image", typeof(string), typeof(AccordionPane), null, propertyChanged: OnImageChanged);
 		public static readonly BindableProperty IsExpandedProperty = BindableProperty.Create("IsExpanded", typeof(bool), typeof(AccordionPane), false);
@@ -83,73 +150,6 @@ namespace Dustbuster
         public virtual void OnPaneCollapsed() { }
 
         public virtual void OnPaneInvalidate() { }
-
-		public class AccordionHeader : ContentView
-		{
-			private RelativeLayout titleLayout;
-			private StackLayout imageHolder;
-			private Label titleLabel;
-			private Image iconImage;
-
-
-			public AccordionHeader(AccordionPane pane)
-			{
-				HeightRequest = 50;
-				HorizontalOptions = LayoutOptions.FillAndExpand;
-
-
-				Content = (titleLayout = new RelativeLayout()
-				{
-					HorizontalOptions = LayoutOptions.FillAndExpand,
-					VerticalOptions = LayoutOptions.FillAndExpand
-				});
-
-				titleLayout.Children.Add((imageHolder = new StackLayout()
-				{
-					VerticalOptions = LayoutOptions.FillAndExpand,
-					WidthRequest = 60
-				}), heightConstraint: Constraint.RelativeToParent((parent) => { return parent.Height; }));
-
-				StackLayout imageContainer;
-				imageHolder.Children.Add(imageContainer = new StackLayout()
-				{
-					HorizontalOptions = LayoutOptions.FillAndExpand,
-					VerticalOptions = LayoutOptions.FillAndExpand,
-					Padding = new Thickness(20, 0, 0, 0)
-				});
-
-				imageContainer.Children.Add((iconImage = new Image()
-				{
-					HeightRequest = 32,
-					HorizontalOptions = LayoutOptions.Center,
-					VerticalOptions = LayoutOptions.CenterAndExpand
-				}));
-				titleLayout.Children.Add((titleLabel = new Label()
-				{
-					HorizontalOptions = LayoutOptions.Center,
-					VerticalOptions = LayoutOptions.Center,
-					HorizontalTextAlignment = TextAlignment.Center,
-					VerticalTextAlignment = TextAlignment.Center,
-					FontSize = 20f,
-					TextColor = Color.White,
-					FontAttributes = FontAttributes.Bold
-				}), widthConstraint: Constraint.RelativeToParent((parent) => { return parent.Width; }),
-					heightConstraint: Constraint.RelativeToParent((parent) => { return parent.Height; }));
-			}
-
-			public string Title
-			{
-				get { return titleLabel.Text; }
-				set { this.titleLabel.Text = value; }
-			}
-
-			public ImageSource IconImage
-			{
-				get { return this.iconImage.Source; }
-				set { this.iconImage.Source = value; }
-			}
-		}
-
 	}
 }
 
