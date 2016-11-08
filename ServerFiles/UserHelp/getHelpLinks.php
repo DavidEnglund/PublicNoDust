1c14c3d80a2a082f84c5e2a3e3a5fc3da02f5472
David Englund <david@David-macbook.local>
2016-11-08 17:14:18 +0800

    fixed errors in get help links, it's now live at www.rainstorm.com.au/app/userHelp/getHelpLinks.php

--- ServerFiles/UserHelp/getHelpLinks.php
+++ ServerFiles/UserHelp/getHelpLinks.php
@@ -1,8 +1,13 @@
 <?PHP
+
 // of course we need all of the connection details and setup
 require_once("../connectToDatabase.php");
+getHelpLinks();
 // this function will get the details and add them to the database to record that a contact request was sent
 function getHelpLinks(){
+connectToDatabase();
+global $databaseConnection;
+
 	//setting up the SQL string
 	 $sql = "SELECT * FROM helpLinks";
 	 // start the return JSON and success boolean
@@ -12,18 +17,20 @@ function getHelpLinks(){
 	try{
 		$returnJSON .= "'links':[";
 		foreach($databaseConnection->query($sql) as $row) {
-			$returnJSON .= "{'id':" $row['id'] . ",'link':'" . $row['link'] . "','title':'" . $row['title'] . "'},";
-			$returnJSON = rtrim($returnJSON, ",");
-			$returnJSON .= "],error:{},";
-			$success = true;
+			$returnJSON .= "{'id':" . $row['id'] . ",'link':'" . $row['link'] . "','title':'" . $row['title'] . "'},";
+			
 		}	
+		$returnJSON = rtrim($returnJSON, ",");
+		$returnJSON .= "],error:{},";
+		$success = true;
 	}
 	catch(PDOException $e){
-		$returnJSON .= "links:{},"
+		$returnJSON .= "links:{},";
 		$returnJSON .= "error:{'errorType':'sql','errorMessage':'" . $e->getMessage() . "'},";
 		$success = false;
-    }	
+    }
 	// now to finish off the return and echo it 
 	$returnJSON .= "'success':" . $success . "}";
+	echo $returnJSON;
 }
 ?>
